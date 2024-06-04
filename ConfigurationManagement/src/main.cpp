0051a258042538e91943d29168f57681460d22ed
VIVEK\vivek <v.cse59@gmail.com>
2024-06-04 11:22:18 -0600

    Update cmakelists.txt to link dynamic libraries compatible with both windows and linux platforms

--- ConfigurationManagement/src/main.cpp
+++ ConfigurationManagement/src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "json/json.h"
 #include "ConcreteClasses/ConfigurationManagement.h"
 #include "ConcreteClasses/JsonConfigParser.h"
 #include "ConcreteClasses/XMLConfigParser.h"
@@ -8,6 +8,26 @@ using namespace Configuration;
 
 int main(int argc, char *argv[])
 {
+    Json::Value root;
+    Json::Value data;
+    constexpr bool shouldUseOldWay = false;
+    root["action"] = "run";
+    data["number"] = 1;
+    root["data"] = data;
+
+    if (shouldUseOldWay)
+    {
+        Json::FastWriter writer;
+        const std::string json_file = writer.write(root);
+        std::cout << json_file << std::endl;
+    }
+    else
+    {
+        Json::StreamWriterBuilder builder;
+        const std::string json_file = Json::writeString(builder, root);
+        std::cout << json_file << std::endl;
+    }
+
     ICONFIG_PARSER_SHARED_PTR configParser = std::make_shared<XMLConfigParser>();
     ICONFIG_MGMT_SHARED_PTR configMgmt = std::make_shared<ConfigurationManagement>(configParser);
 
